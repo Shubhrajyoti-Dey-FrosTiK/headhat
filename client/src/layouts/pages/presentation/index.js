d314391d7c475a23a11333304be9768434ab0c13
Shubhrajyoti Dey <shubhrajyotidey@Apples-MacBook-Pro.local>
2022-10-15 14:20:06 +0530

    Added checkLogggedInSupport

--- client/src/layouts/pages/presentation/index.js
+++ client/src/layouts/pages/presentation/index.js
@@ -15,7 +15,37 @@ Coded by www.creative-tim.com
 
 // Material Kit 2 React pages
 import Presentation from "pages/Presentation";
+import { useEffect } from "react";
+import useEth from "../../../contexts/EthContext/useEth";
 
 export default function PresentationPage() {
+  const {
+    state: { contract, accounts },
+  } = useEth();
+
+  const read = async () => {
+    const loginStatus = await contract.methods
+      .getUserLoginStatus()
+      .call({ from: accounts[0] });
+    console.log(loginStatus);
+    if (!Number(loginStatus)) {
+      const createUser = await contract.methods
+        .addUser("SD", "Hello")
+        .send({ from: accounts[0] });
+      console.log(createUser);
+      console.log("User should be created");
+    } else {
+      console.log("User exists");
+    }
+
+    // const value = await contract.methods
+    //   .getUserLoginStatus()
+    //   .call({ from: accounts[0] });
+    console.log(contract);
+  };
+
+  useEffect(() => {
+    if (contract) read();
+  }, [contract]);
   return <Presentation />;
 }
